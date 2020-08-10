c6d69d3b7fc5dfc577e75ec3feb892fbed6b0b0f
ajiir <ajiir2247@gmail.com>
2020-08-10 16:57:43 +0800

    Client area feature-iig hiij duuslaa

---
+++ app/assets/scripts/modules/ClientArea.js
@@ -0,0 +1,53 @@
+import Axios from "axios";
+
+class ClientArea {
+  constructor() {
+    this.injectHTML();
+    this.form = document.querySelector(".client-area_form");
+    this.form = document.querySelector(".client-area_input");
+    this.contentArea = document.querySelector(".client-area__content-area");
+    this.events();
+  }
+
+  events() {
+    this.form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      this.sendRequest();
+    });
+  }
+
+  sendRequest() {
+    Axios.post("https://ajiir.netlify.app/.netlify/functions/secret-page", {
+      password: this.field.value,
+    })
+      .then((response) => {
+        this.form.remove();
+        this.contentArea.innerHTML = response.data;
+      })
+      .catch(() => {
+        this.contentArea.innerHTML = `<p class="client-area__error">Таны оруулсан нууц үг буруу байна. Дахин оролдоно уу!</p>`;
+        this.field.value = "";
+        this.field.focus();
+      });
+  }
+
+  injectHTML() {
+    document.body.insertAdjacentHTML(
+      "beforeend",
+      `
+        <div class="client-area">
+        <div class="wrapper wrapper--medium">
+        <h2 class="section-title section-title--blue">Хэрэглэгчийн тусгай хуудас</h2>
+        <form action="" class="client-area__form">
+        <input type="text" class="client-area__input" placeholder="Нууц үгээ оруулна уу?">
+        <button class="btn btn--orange">Нэвтрэх</button>
+        </form>
+        <div class="client-area__content-area"></div>
+        </div>
+        </div>
+        `
+    );
+  }
+}
+
+export default ClientArea;
